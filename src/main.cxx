33edc2707cf1d5e6d1f44f38803671d0c197358f
Cem GEÇGEL <gecgelcem@outlook.com>
2023-04-20 20:29:07 +0300

    refactor: Arguments as a Struct

--- src/main.cxx
+++ src/main.cxx
@@ -5,52 +5,47 @@
 #include <iostream>
 #include <span>
 #include <stdexcept>
-#include <string_view>
 
 /// Parsed command-line arguments.
 struct Arguments
 {
-  explicit Arguments(std::span<char const* const> commandLineArguments)
+  std::filesystem::path mainDirectory;
+  std::string mainFile;
+
+  static Arguments parse(std::span<char const* const> commandLineArguments)
   {
     switch (commandLineArguments.size())
     {
     case 0:
     case 1:
-      throw std::runtime_error(
+      throw std::invalid_argument(
         "Usage: rainfall [main-directory: .] <main-file>");
     case 2:
-      mainDirectory = ".";
-      mainFile = commandLineArguments[1];
-      break;
+      return Arguments{".", commandLineArguments[1]};
     case 3:
-      mainDirectory = commandLineArguments[1];
-      mainFile = commandLineArguments[2];
-      break;
+      return Arguments{
+        std::filesystem::path(commandLineArguments[1]).lexically_normal(),
+        commandLineArguments[2]};
     default:
-      throw std::runtime_error(
+      throw std::invalid_argument(
         "There could be at most 2 command line arguments!");
     }
-
-    mainDirectory = mainDirectory.lexically_normal();
   }
 
-  rf::Source loadMainSource()
+  rf::Source loadMainSource() const
   {
     return rf::Source::load(mainDirectory, mainFile);
   }
-
-private:
-  std::filesystem::path mainDirectory;
-  std::string mainFile;
 };
 
 /// Entry into the compiler.
-int main(int const argc, char const* const* const argv)
+int main(int argc, char const* const* argv)
 {
   try
   {
-    auto arguments = Arguments(std::span(argv, argc));
+    auto arguments = Arguments::parse(std::span(argv, argc));
     auto mainSource = arguments.loadMainSource();
+
     std::cout << "Source: " << mainSource.name << " in " << mainSource.directory
               << std::endl
               << "Full Path: " << mainSource.fullPath << std::endl
