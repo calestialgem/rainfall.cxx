b0d3239ef971beb43698a52ff7d64b633e19f5b1
Cem GEÇGEL <gecgelcem@outlook.com>
2023-04-28 14:16:05 +0300

    feat: Add Parsed Source Types

--- src/parser.cxx
+++ src/parser.cxx
@@ -2,14 +2,145 @@
 
 #include "lexer.cxx"
 
+#include <memory>
 #include <utility>
+#include <variant>
 #include <vector>
 
 namespace rf
 {
+  /// Instructions to compute a value.
+  struct Expression
+  {
+    struct Literal
+    {
+      Number number;
+    };
+
+    struct PrefixOperation
+    {
+      enum Variant
+      {
+        Promotion,
+        Negation,
+        BitwiseNot,
+        LogicalNot,
+      };
+
+      std::unique_ptr<Expression> operand;
+      Variant variant;
+    };
+
+    struct InfixOperation
+    {
+      enum Variant
+      {
+        Multiplication,
+        Division,
+        Reminder,
+        Addition,
+        Subtraction,
+        BitwiseAnd,
+        BitwiseOr,
+        BitwiseXor,
+        LeftShift,
+        RightShift,
+        Equal,
+        NotEqual,
+        Less,
+        Greater,
+        LessOrEqual,
+        GreaterOrEqual,
+        LogicalAnd,
+        LogicalOr,
+      };
+
+      std::unique_ptr<Expression> rightOperand;
+      std::unique_ptr<Expression> leftOperand;
+      Variant variant;
+    };
+
+    using Variant = std::variant<Literal, PrefixOperation, InfixOperation>;
+
+    Variant variant;
+  };
+
+  /// Applications to construct a type.
+  struct Formula
+  {
+    struct Const
+    {
+    };
+
+    struct Mut
+    {
+    };
+
+    struct Ref
+    {
+    };
+
+    struct RefMut
+    {
+    };
+
+    struct Plain
+    {
+    };
+
+    using Qualifier = std::variant<Const, Mut, Ref, RefMut, Plain>;
+
+    struct Inferred
+    {
+    };
+
+    struct Named
+    {
+      Identifier name;
+    };
+
+    using Base = std::variant<Inferred, Named>;
+
+    Qualifier qualifier;
+    Base base;
+  };
+
+  /// Pattern, which can be matched to a value or bind a value to a variable.
+  struct Pattern
+  {
+    struct Type
+    {
+      Formula expected;
+    };
+
+    struct Binding
+    {
+      Type type;
+      Identifier name;
+    };
+
+    struct Matching
+    {
+      Expression matched;
+    };
+
+    using Variant = std::variant<Type, Binding, Matching>;
+
+    Variant variant;
+  };
+
   /// Entities in a Thrice program that define the symbols in the program.
   struct Definition
   {
+    struct Binding
+    {
+      Pattern pattern;
+      Expression bound;
+    };
+
+    using Variant = std::variant<Binding>;
+
+    Variant variant;
   };
 
   /// Representation of a Thrice source file that when through the syntactical
