c16f2b9ca72a63da92326fc552d744bd2bbdc65b
Cem GEÇGEL <gecgelcem@outlook.com>
2023-06-16 17:42:31 +0300

    refactor: Using C Headers

--- src/main.cxx
+++ src/main.cxx
@@ -1,8 +1,9 @@
-#include <cstdio>
+#include <stdio.h>
+#include <stdlib.h>
 
 int main(int const argument_count, char const* const* const arguments) {
   for (int i = 0; i < argument_count; i++) {
-    (void)std::fprintf(stderr, "[%i] %s\n", i, arguments[i]);
+    (void)fprintf(stderr, "[%i] %s\n", i, arguments[i]);
   }
-  return 0;
+  return EXIT_SUCCESS;
 }
